1410a4de39b4d63cb9ff49e793a3b6fcab6d02d7
Ivan Shevtsov <vanyashevcov@ukr.net>
2025-03-05 16:02:16 +0200

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import ContactList from './components/ContactList/ContactList.jsx';
-import SearchBox from './components/SearchBox/SearchBox.jsx';
-import ContactForm from './components/ContactForm/ContactForm.jsx';
+import { useEffect, useState } from "react";
+import "./App.css";
+import ContactList from "./components/ContactList/ContactList.jsx";
+import SearchBox from "./components/SearchBox/SearchBox.jsx";
+import ContactForm from "./components/ContactForm/ContactForm.jsx";
 
 function App() {
-  const [userData, setUserData] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const getInitialContacts = () => {
+    const savedContacts = localStorage.getItem("userData");
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  };
+
+  const [userData, setUserData] = useState(getInitialContacts);
 
   const [filter, setFilter] = useState("");
 
-  const filteredContacts = userData.filter(
-    (contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-      contact.number.toLowerCase().includes(filter.toLowerCase())
+   useEffect(() => {
+     localStorage.setItem("userData", JSON.stringify(userData));
+   }, [userData]);
+
+  const filteredContacts = userData.filter(({ name, number }) =>
+    `${name} ${number}`.toLowerCase().includes(filter.toLowerCase())
   );
-  
+
   const addContact = (contact) => {
     setUserData((prev) => [...prev, contact]);
   };
@@ -40,4 +49,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
